2fdc4eb1f12d1169efa4c917466364c2f55c0a95
Boris <rozumniy.boris.work@gmail.com>
2021-11-04 18:00:09 +0200

    init project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require(
+  './authRouter'
+);
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use('/auth', authRouter);
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://boris:qwer1234@cluster0.qqjou.mongodb.net/authorization_roles?retryWrites=true&w=majority'
+    );
+    app.listen(PORT, () =>
+      console.log(
+        `server started on port ${PORT}`
+      )
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();
