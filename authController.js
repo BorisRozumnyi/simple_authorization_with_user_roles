929ff083a63e2ce8748eaf773590c717af955240
Boris <rozumniy.boris.work@gmail.com>
2021-11-08 18:25:11 +0200

    added validation

--- authController.js
+++ authController.js
@@ -1,31 +1,55 @@
 const User = require('./models/User');
 const Role = require('./models/Role');
 const bcrypt = require('bcrypt');
+const {
+  validationResult,
+} = require('express-validator');
 
 class authController {
   async registration(req, res) {
     try {
-      const { username, password } = req.body;
-      const candidate = await User.findOne({ username });
-      if (candidate) {
-        return res.status(400).json({ message: 'An account with the same name already exists'});
+      // console.log(authorization_roles.users.getIndexes())
+      const errors =
+        validationResult(req);
+      if (!errors.isEmpty) {
+        return res.status(400).json({
+          message: 'Registration error',
+          errors,
+        });
       }
-      const hashPassword = bcrypt.hashSync(password, 7);
-      const userRole = await Role.findOne({ value: 'USER' });
+      const { username, password } =
+        req.body;
+      const candidate =
+        await User.findOne({
+          username,
+        });
+      if (candidate) {
+        return res.status(400).json({
+          message:
+            'An account with the same name already exists',
+        });
+      }
+      const hashPassword =
+        bcrypt.hashSync(password, 7);
+      const userRole =
+        await Role.findOne({
+          value: 'USER',
+        });
       const user = new User({
         username,
         password: hashPassword,
         roles: [userRole.value],
       });
-      console.log(
-        userRole.value, user
-        )
+      // console.log(userRole.value, user);
       await user.save();
-      return res.json({ message: 'User registered successfully' });
+      return res.json({
+        message:
+          'User registered successfully',
+      });
     } catch (e) {
-      console.log(e.message);
+      console.log('e.message');
       res.status(400).json({
-        message: 'Refistration error',
+        message: e.message,
       });
     }
   }
--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,28 @@
 const Router = require('express');
 const router = new Router();
-const controller = require('./authController')
+const controller = require('./authController');
+const {
+  check,
+} = require('express-validator');
 
-router.post('/registration', controller.registration);
+router.post(
+  '/registration',
+  [
+    check(
+      'username',
+      'Username cannot be empty'
+    ).notEmpty(),
+    check(
+      'Password',
+      'Password should be more than 4 and less than 10 symbols'
+    ).isLength({ min: 4, max: 10 }),
+  ],
+  controller.registration
+);
 router.post('/login', controller.login);
-router.get('/users', controller.getUsers);
+router.get(
+  '/users',
+  controller.getUsers
+);
 
 module.exports = router;
