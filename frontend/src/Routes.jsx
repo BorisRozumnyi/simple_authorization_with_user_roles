74c347c1fca6cfd41901ca4c8479cc52f3514093
Boris <rozumniy.boris.work@gmail.com>
2021-11-11 10:35:19 +0200

    added routing

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes as RoutesRRD,
+  Route,
+  Link,
+} from 'react-router-dom';
+import { UserList } from './components';
+import { Login } from './components';
+
+export const Routes = () => {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link to="/login">
+              login
+            </Link>
+          </li>
+          <li>
+            <Link to="/users">
+              users
+            </Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <RoutesRRD>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/dashboard"
+            element={<Dashboard />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="/users"
+            element={<UserList />}
+          />
+        </RoutesRRD>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { UserList } from './components/UserList';
-import { Login } from './components/Login';
 import reportWebVitals from './reportWebVitals';
+import { Routes } from './Routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
-    {/* <UserList /> */}
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
