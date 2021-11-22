3537f3ddb23eaa55344d89836ee4ce6d969f69b3
Boris <rozumniy.boris.work@gmail.com>
2021-11-22 18:57:13 +0200

    corrected export for the Home

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -8,11 +8,11 @@ import {
   Registration,
   Login,
   UserList,
+  Home,
 } from './pages';
 import { Container } from './components';
 import { frontend } from './urls';
 import { Header } from './components/Header';
-import { Home } from './pages/Home';
 
 export const Routes = () => {
   return (
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,4 @@
+export * from './Home';
 export * from './Registration';
 export * from './Login';
 export * from './UserList';
